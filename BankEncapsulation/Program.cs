29203a9f9485838f063fd9c254a73f05a55326b8
xmilak <126503532+xmilak@users.noreply.github.com>
2023-03-20 13:19:22 -0400

    FINISHED

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+            Console.WriteLine("Welcome to the bank");
+
+            while (true)
+            {
+                Console.WriteLine("Enter 'D' to deposit money or 'B' to retrieve your balance");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "d")
+                {
+                    Console.WriteLine("Enter deposit amount");
+                    double amount = double.Parse(Console.ReadLine());
+                    account.Deposit(amount);
+                    Console.WriteLine("Deposit successful");
+                }
+                else if (input.ToLower() == "b")
+                {
+                    double balance = account.GetBalance();
+                    Console.WriteLine($"Your current balance is: {balance:C}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input please try again");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
